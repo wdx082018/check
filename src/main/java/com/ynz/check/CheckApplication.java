9bf64518e4ad80554e7cdf2a5d670ec2b2806011
wdx082018 <wdx138861@163.com>
2018-08-05 14:34:18 +0800

    check

---
+++ src/main/java/com/ynz/check/CheckApplication.java
@@ -0,0 +1,21 @@
+package com.ynz.check;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class CheckApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(CheckApplication.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(CheckApplication.class, args);
+    }
+
+
+}
---
+++ src/main/java/com/ynz/check/controller/CheckAotai.java
@@ -0,0 +1,25 @@
+package com.ynz.check.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+@RequestMapping(value = "/check")
+public class CheckAotai {
+
+    @Value("${aotai.status}")
+    private Integer status;
+
+    @RequestMapping(value = "/login")
+    @ResponseBody
+    public Object checkLogin() {
+        Map<String, Object> result = new HashMap<>();
+        result.put("status", status);
+        return result;
+    }
+}
